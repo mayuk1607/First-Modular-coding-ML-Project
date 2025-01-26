9097c405234e346dd11559dca32a70a527bc9229
mayuk1607 <mayuk1607@gmail.com>
2025-01-26 17:33:01 +0000

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,6 @@
 import logging
 import os
-import sys
 from datetime import datetime
-from src.exception import CustomException
 
 log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",log_file)
@@ -16,13 +14,5 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-if __name__ =="__main__":
-
-    try:
-        a=1/0
-
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
 
 
